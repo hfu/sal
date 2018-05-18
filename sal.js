4364458913382bd8d7017bbea0ebe5bd71052f53
Hidenori Fujimura <hfu@users.noreply.github.com>
2018-05-18 15:06:27 +0000

    update

--- sal.js
+++ sal.js
@@ -1,9 +1,11 @@
 const config = require('config')
 const fs = require('fs')
 const shapefile = require('shapefile')
+const turf = require('@turf/turf')
 
 const convert = (path, t) => {
-  shapefile.open(path).then(s => s.read()
+  shapefile.open(path, path.replace('shp', 'dbf'), {encoding: 'utf-8'})
+  .then(s => s.read()
     .then(function proc(r) {
       if (r.done) return
       let f = r.value
@@ -11,6 +13,11 @@ const convert = (path, t) => {
       delete f.properties.Shape_Area
       f.tippecanoe = {minzoom: 2, maxzoom: 8, layer: t.toLowerCase()}
       console.log(JSON.stringify(f))
+      if (t === 'BNDA') {
+        let f2 = turf.centerOfMass(f.geometry, f.properties)
+        f2.tippecanoe = {minzoom: 2, maxzoom: 8, layer: 'bndp'}
+        console.log(JSON.stringify(f2))
+      }
       return s.read().then(proc)
     }))
   .catch(err => console.error(err.stack))
